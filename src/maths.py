1bad1972b53c5a6ebf5a17a9411dd7955f7e29df
Nausher Rao <SherRao15@gmail.com>
2024-02-27 19:27:17 -0500

    Start work on maths functions

---
+++ src/maths.py
@@ -0,0 +1,11 @@
+# write a function that calculates the wedge product of two vector.
+def wedge_product(v1, v2):
+    return v1[0] * v2[1] - v1[1] * v2[0]
+
+# write a function that calculates the dot product of two vectors.
+def dot_product(v1, v2):
+    return v1[0] * v2[0] + v1[1] * v2[1]
+
+# write a function that calculates the cross product of two vectors.
+def cross_product(v1, v2):
+    return v1[0] * v2[1] - v1[1] * v2[0]
